9a7830647ac0641df7531d1fc9073ac5e104557f
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-03-08 11:45:30 +0100

    ✨ Hero banner

---
+++ resources/views/components/hero-banner.blade.php
@@ -0,0 +1,17 @@
+<div class="relative flex w-full">
+    <img class="aspect-[960/449]" src="{{ asset("assets/banner.png") }}" alt="Hero banner" loading="lazy">
+    <div class="absolute top-0 left-0 w-full h-full flex items-center container mx-auto p-5 lg:p-10">
+        <div class="max-w-[530px] translate-x-1/2">
+            <span class="font-yellowtail text-ds-green text-h4 mb-2">100% Natural Food</span>
+            <h1 class="text-h1 mb-5">Choose the best healthier way of life</h1>
+            <a href="#" class="btn btn--secondary">
+                <span>Explore Now</span>
+                <div>
+                    <svg width="9" height="8" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="m4.476 1.129 3.395 3.064-3.395 3.065M7.4 4.193H.516" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
