e36974c97384f884bd7d5073c5d22e76e03089c3
lucas <luqui31@gmail.com>
2024-03-08 09:53:05 +0100

    migrations

---
+++ database/migrations/2024_03_08_083927_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->decimal('price_sale', 10, 2);
+            $table->integer('note');
+            $table->string('image');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
