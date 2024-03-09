cbd13f0eb00dfa57967098aa4ff9c3376187e8f4
Valentin Muller <valmuller14@gmail.com>
2024-03-09 10:25:00 +0100

    add about us section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,6 @@
 <x-guest-layout>
     <x-slot name="slot">
-       <div>
-            <h1>Welcome Home</h1>
-       </div>
        <x-hero-banner/>
+       <x-aboutus/>
     </x-slot> 
 </x-guest-layout>
