40b20d8bd4e444a402273d0c5fda007d42748ad8
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-03-22 11:01:21 +0100

    🐛 Logo header

--- resources/views/components/eco-friendly.blade.php
+++ resources/views/components/eco-friendly.blade.php
@@ -1,7 +1,7 @@
 @if (isset($inIframe) && $inIframe)
     @extends('layouts.component')
     @section('content')
-@endif
+    @endif
     <section class="relative flex justify-center w-full">
         <div class="w-1/2 h-[930px]">
             <img src="{{ asset('assets/eco-friendly.jpg') }}" alt="Eco Friendly" class="object-cover object-center">
@@ -14,20 +14,23 @@
                 </h2>
                 <p class="font-medium text-2xl text-ds-blue ">Start with Our Company First</p>
                 <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed ut perspiciatis.
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed
+                    ut perspiciatis.
                 </p>
-                <p class="font-medium text-2xl text-ds-blue mt-9" >Learn How to Grow Yourself</p>
+                <p class="font-medium text-2xl text-ds-blue mt-9">Learn How to Grow Yourself</p>
                 <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed ut perspiciatis.
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed
+                    ut perspiciatis.
                 </p>
                 <p class="font-medium text-2xl text-ds-blue mt-9">Farming Strategies of Today</p>
                 <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed ut perspiciatis.
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium <br> doloremque laudantium. Sed
+                    ut perspiciatis.
                 </p>
             </div>
 
         </div>
     </section>
-@if (isset($inIframe) && $inIframe)
+    @if (isset($inIframe) && $inIframe)
     @endsection
 @endif
--- resources/views/components/offer-banner.blade.php
+++ resources/views/components/offer-banner.blade.php
@@ -1,29 +1,31 @@
 @if (isset($inIframe) && $inIframe)
     @extends('layouts.component')
     @section('content')
-@endif
+    @endif
 
     <section class="relative flex justify-center w-full p-14">
         <article class="flex items-center max-w-[1390px] gap-9">
             <div>
-            <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36"   style="background-image: url('{{ asset("assets/offer1.png") }}')">
-                <div class="">
-                <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 1</p>
-                <p class="text-lg text-white text-h3">Sous-titre de l'image 1</p>
+                <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36"
+                    style="background-image: url('{{ asset('assets/offer1.png') }}')">
+                    <div class="">
+                        <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 1</p>
+                        <p class="text-lg text-white text-h3">Sous-titre de l'image 1</p>
+                    </div>
                 </div>
-            </div>
             </div>
             <div>
-            <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36 "  style="background-image: url('{{ asset("assets/offer2.png") }}')">
-                <div>
-                <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 2</p>
-                <p class="text-lg text-ds-blue text-h3">Sous-titre de l'image 2</p>
+                <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36 "
+                    style="background-image: url('{{ asset('assets/offer2.png') }}')">
+                    <div>
+                        <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 2</p>
+                        <p class="text-lg text-ds-blue text-h3">Sous-titre de l'image 2</p>
+                    </div>
                 </div>
-            </div>
             </div>
         </article>
     </section>
 
-@if (isset($inIframe) && $inIframe)
+    @if (isset($inIframe) && $inIframe)
     @endsection
 @endif
