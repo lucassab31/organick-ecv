6fb85ccf0ad541b552fca7b4e8ec63291fdb0ebd
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-03-22 09:19:32 +0100

    ✨ Pull from origin/main and shop section done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-slot name="slot">
-       <x-hero-banner :content="$content" />
-       <x-aboutus />
-       <x-news />
-    </x-slot> 
+        <x-hero-banner :content="$content" />
+        <x-aboutus />
+        <x-news />
+        <x-shop />
+    </x-slot>
 </x-guest-layout>
