e4d10f9bcfce8ada879c13862a3ba22ab3011408
Valentin Muller <valmuller14@gmail.com>
2024-03-22 11:10:07 +0100

    update offer banner

--- resources/views/components/offer-banner.blade.php
+++ resources/views/components/offer-banner.blade.php
@@ -5,24 +5,34 @@
 
     <section class="relative flex justify-center w-full p-14">
         <article class="flex items-center max-w-[1390px] gap-9">
-            <div>
-            <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36"   style="background-image: url('{{ asset("assets/offer1.png") }}')">
-                <div class="">
-                <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 1</p>
-                <p class="text-lg text-white text-h3">Sous-titre de l'image 1</p>
-                </div>
+          <div class="relative">
+            <picture class="aspect-[960/449]" loading="lazy">
+                <source
+                    srcset="{{ isset($content->offer_banner->image) ? asset('storage/images/webp/' . pathinfo($content->offer_banner->image, PATHINFO_FILENAME) . '.webp') : '' }}"
+                    type="image/webp">
+                <img src="{{ isset($content->offer_banner->image) ? asset('storage/images/original/' . $content->offer_banner->image) : '' }}"
+                    alt="Hero banner" />
+            </picture>
+            <div class="absolute top-1/2 -translate-y-1/2 left-10">
+              <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 1</p>
+              <p class="text-lg text-white text-h3">Sous-titre de l'image 1</p>      
             </div>
+          </div>
+          <div class="relative">
+            <picture class="aspect-[960/449]" loading="lazy">
+                <source
+                    srcset="{{ isset($content->offer_banner->image) ? asset('storage/images/webp/' . pathinfo($content->offer_banner->image, PATHINFO_FILENAME) . '.webp') : '' }}"
+                    type="image/webp">
+                <img src="{{ isset($content->offer_banner->image) ? asset('storage/images/original/' . $content->offer_banner->image) : '' }}"
+                    alt="Hero banner" />
+            </picture>
+            <div class="absolute top-1/2 -translate-y-1/2 left-10">
+              <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 2</p>
+              <p class="text-lg text-ds-blue text-h3">Sous-titre de l'image 2</p>
             </div>
-            <div>
-            <div class="bg-cover bg-center rounded-lg h-[360px] flex items-center pl-16 pr-36 "  style="background-image: url('{{ asset("assets/offer2.png") }}')">
-                <div>
-                <p class="font-yellowtail text-ds-green text-h4 mb-2">Titre de l'image 2</p>
-                <p class="text-lg text-ds-blue text-h3">Sous-titre de l'image 2</p>
-                </div>
-            </div>
-            </div>
+          </div>
         </article>
-    </section>
+    </section>      
 
 @if (isset($inIframe) && $inIframe)
     @endsection
